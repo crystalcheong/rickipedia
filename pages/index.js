87ccf75425aee23f77515199d307487cc5358be0
Crystal Cheong <crystal.cheongyuqing@gmail.com>
2021-03-27 22:36:39 +0800

    API data population + index page skeleton

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,72 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+const defaultEndpoint = `https://rickandmortyapi.com/api/character/`;
+
+export async function getServerSideProps() {
+  const res = await fetch(defaultEndpoint)
+  const data = await res.json()
+  return {
+    props: {
+      data
+    }
+  }
+}
+
+export default function Home({ data }) {
+
+  console.log(data)
+
+  const { results = [] } = data;
+  console.log(results)
+
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Rickipedia</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Wubba Lubba <span>Dub Dub!</span>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Rick and Morty Character Wiki
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <ul className={styles.grid}>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          {
+            results.map((result) => {
+              const { id, name, image } = result;
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+              return <Link href={`#${name.replace(/\s+/g, '')}`} passHref key={id}>
+                <li className={styles.card}>
+                <Image
+                src={image}
+                alt={`${name} Thumbnail`}
+                width={200}
+                height={200}
+                />
+                  <h3>{name}</h3>
+                </li>
+              </Link>
+            })
+          }
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+
+        </ul>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <Link href="https://www.adultswim.com/" passHref>
+        <img src="/adult-swim.svg" alt="Vercel Logo" className={styles.logo} />
+        </Link>
       </footer>
     </div>
   )
